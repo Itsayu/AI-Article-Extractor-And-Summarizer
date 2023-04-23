17727638f84b188df0cc44d0df32dea7b543f1db
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2023-04-23 21:03:13 +0530

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='SAWO_logo' className='w-28 object-contain' style={{width:"11rem"}} />
+        <img src={logo} alt='SAWO_logo' className='w-28 object-contain' style={{width:"12rem"}} />
 
         <button
           type='button'
